0c0e8d95825c9641da6e2b45d1e16d7e742fbf28
Tom <tomlegnard@gmail.com>
2020-09-04 23:28:05 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml;
+using Newtonsoft.Json;
+//using System.IO;
+
+//using JsonConvert;
+// using XmlDocument;
+
+namespace ccd_parsing
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            XmlDocument doc = new XmlDocument();
+            doc.Load("ccd-sample.xml");
+            string xmlcontents = doc.InnerXml;
+            string json = JsonConvert.SerializeXmlNode(doc);
+
+            Console.WriteLine("writing json");
+            System.IO.File.WriteAllText("ccd-sample.json", json);
+            Console.WriteLine("json write complete.");
+        }
+    }
+}
